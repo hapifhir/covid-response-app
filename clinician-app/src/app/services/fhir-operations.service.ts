ae9b503157aa86ecc4fcecb20322ff5882b06ac4
Jorge Martin <martin76.mail@gmail.com>
2020-05-04 22:48:54 -0500

    cleaned fhir-operations.service.ts

--- clinician-app/src/app/services/fhir-operations.service.ts
+++ clinician-app/src/app/services/fhir-operations.service.ts
@@ -9,16 +9,8 @@ export class FhirOperationsService {
 
   constructor() { }
   
-  generateQuestionnaireResponse(formValues,quest?:any)
-  {
-    if (quest===undefined || quest===null)
-      this.generateQuestionnaireResponseBasic(formValues);
-    else
-      this.generateQuestionnaireResponseFull(formValues,quest);
-
-  }
-
-  generateQuestionnaireResponseFull (formValues,quest:any) {
+ 
+  generateQuestionnaireResponse (formValues,quest:any) {
     const scope = this;
     const questionnaireResponse = new FHIR.QuestionnaireResponse();
     const identifier = new FHIR.Identifier();
@@ -34,7 +26,7 @@ export class FhirOperationsService {
       const questionnaireResponseGroupItem = scope.generateQuestionnaireResponseGroupItem(elementGroup.linkId , elementGroup.text);
       questionnaireResponseGroupItem.item =[];
       elementGroup.item.forEach(elementItem=>{
-        const questionnaireResponseItem = scope.generateQuestionnaireResponseItemFull(elementItem,elementGroup.linkId + '_' + elementItem.linkId, formValues[elementGroup.linkId][elementItem.linkId]);
+        const questionnaireResponseItem = scope.generateQuestionnaireResponseItem(elementItem,elementGroup.linkId + '_' + elementItem.linkId, formValues[elementGroup.linkId][elementItem.linkId]);
         if(questionnaireResponseItem !== null) {
           questionnaireResponseGroupItem.item.push(questionnaireResponseItem);
         }
@@ -45,50 +37,7 @@ export class FhirOperationsService {
     
     return questionnaireResponse;
   }
-  generateQuestionnaireResponseBasic(formValues) {
-    const scope = this;
-    const questionnaireResponse = new FHIR.QuestionnaireResponse();
-    const identifier = new FHIR.Identifier();
-    identifier.value = 'SARS_Questionnaire_Response';
-    questionnaireResponse.identifier = identifier;
-    questionnaireResponse.resourceType = 'QuestionnaireResponse'
-    questionnaireResponse.status = 'completed';
-    questionnaireResponse.authored = new Date();
-    questionnaireResponse.item = [];
-    Object.keys(formValues).forEach(function(parentKey) {
-      Object.keys(formValues[parentKey]).forEach(function(childKey) {
-        const questionnaireResponseItem = scope.generateQuestionnaireResponseItem(parentKey + '_' + childKey, formValues[parentKey][childKey]);
-        if(questionnaireResponseItem !== null) {
-          questionnaireResponse.item.push(questionnaireResponseItem);
-        }
-      }) 
-  });
-    return questionnaireResponse;
-  }
-
-  generateQuestionnaireResponseItem(linkId, childItem) {
-    let returnItem = null;
-    if(childItem) {
-      const questionnaireResponseItem = new FHIR.QuestionnaireResponseItem();
-      const questionnaireAnswer = new FHIR.Answer();
-      questionnaireResponseItem.answer = [];
-      if(linkId.includes("_bool")) {
-        questionnaireAnswer.valueBoolean = childItem === "yes" ? true : false;
-      } else if(linkId.includes("_choice")) {
-        questionnaireAnswer.valueCoding = new FHIR.Coding();
-        questionnaireAnswer.valueCoding.code = childItem;
-      } else if(linkId.includes("_date")) {
-        questionnaireAnswer.valueDate = new Date(childItem);
-      } else {
-        questionnaireAnswer.valueString = childItem;
-      }
-      questionnaireResponseItem.linkId = linkId;
-      questionnaireResponseItem.answer.push(questionnaireAnswer);
-      returnItem = questionnaireResponseItem
-    }
-    return returnItem;
-  }
-
+ 
   generateQuestionnaireResponseGroupItem(linkId, text) {
     const questionnaireResponseGroupItem = new FHIR.QuestionnaireResponseGroupItem();
     questionnaireResponseGroupItem.linkId = linkId;
@@ -96,7 +45,7 @@ export class FhirOperationsService {
     return questionnaireResponseGroupItem;
   }
  
-  generateQuestionnaireResponseItemFull(elementItem,linkId, childItem) {
+  generateQuestionnaireResponseItem(elementItem,linkId, childItem) {
     let returnItem = null;
     if(childItem) {
       const questionnaireResponseItem = new FHIR.QuestionnaireResponseItem();
