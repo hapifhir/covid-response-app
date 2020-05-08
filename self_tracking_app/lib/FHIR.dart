556b8d9b0edfd581519f36b7aa2dd026ade4fb34
ad1306 <adityadave.dave89@gmail.com>
2020-05-08 13:28:00 -0400

    added Questionnaire & QuestionnaireResponse FHIR resources

--- self_tracking_app/lib/FHIR.dart
+++ self_tracking_app/lib/FHIR.dart
@@ -1,14 +1,15 @@
-library fhir_r4;
 
 class FHIRElement {
   String id;
 }
 
 class BackboneElement extends FHIRElement {
+  BackboneElement();
   var modifierExtension;
 }
 
 class Code extends FHIRElement {
+  Code();
   String codeString;
 }
 
@@ -145,3 +146,266 @@ class Patient extends Resource {
   Reference managingOrganization;
   List<Link> link;
 }
+
+class QuestionnaireResponseItem extends BackboneElement {
+  String linkId;
+  String definition;
+  String text;
+  List<Answer> answer;
+}
+
+class Answer extends BackboneElement {
+  bool valueBoolean;
+  double valueDecimal;
+  int valueInteger;
+  DateTime valueDate;
+  DateTime valueDateTime;
+  DateTime valueTime;
+  String valueString;
+  String valueUri;
+  Attachment valueAttachment;
+  Coding valueCoding;
+  Quantity valueQuantity;
+  Reference valueReference;
+}
+
+class Attachment extends FHIRElement {
+  Code contentType;
+  Code language;
+  String data;
+  String url;
+  double size;
+  String hash;
+  String title;
+  String creation;
+}
+
+class Quantity extends FHIRElement {
+  double value;
+  Code comparator;
+  String unit;
+  String system;
+  Code code;
+}
+
+class Questionnaire extends Resource {
+  String url;
+  List<Identifier> identifier;
+  String version;
+  String name;
+  String title;
+//  derivedFrom: Questionnaire;
+  Code status;
+  bool experimental;
+  List<Code> subjectType;
+  DateTime date;
+  String publisher;
+  List<ContactDetail> contact;
+  String description;
+  List<UsageContext> useContext;
+  List<CodeableConcept> jurisdiction;
+  String purpose;
+  String copyright;
+  DateTime approvalDate;
+  DateTime lastReviewDate;
+  Period effectivePeriod;
+  List<Code> code;
+  List<Item> item;
+}
+
+class QuestionnaireResponse extends Resource {
+  Identifier identifier;
+  List<Reference> basedOn;
+  List<Reference> parent;
+  String questionnaire;
+  String status;
+  Reference context;
+  DateTime authored;
+  Reference author;
+  Reference source;
+  List<QuestionnaireResponseItem> item;
+  Reference subject;
+}
+
+
+class Item extends BackboneElement {
+  String linkId;
+  String definition;
+  List<Coding> code;
+  String prefix;
+  String text;
+  Code type;
+  List<EnableWhen> enableWhen;
+  Code enableBehavior;
+  bool required;
+  bool repeats;
+  bool readOnly;
+  int maxLength;
+  ValueSet answerValueSet;
+  List<AnswerOption> answerOption;
+  List<Initial> initial;
+  List<Item> item;
+}
+
+class Initial extends BackboneElement {
+  bool valueBoolean;
+  double valueDecimal;
+  int valueInteger;
+  DateTime valueDate;
+  DateTime valueDateTime;
+  DateTime valueTime;
+  String valueString;
+  String valueUri;
+  Attachment valueAttachment;
+  Coding valueCoding;
+  Quantity valueQuantity;
+  Reference valueReference;
+}
+
+class EnableWhen extends BackboneElement {
+  String question;
+  String operator;
+  bool answerBoolean;
+  double answerDecimal;
+  int answerInteger;
+  DateTime answerDate;
+  DateTime answerDateTime;
+  DateTime answerTime;
+  String answerString;
+  Coding answerCoding;
+  Quantity answerQuantity;
+  Reference answerReference;
+}
+
+class ValueSet extends DomainResource {
+  String url;
+  List<Identifier> identifier;
+  String version;
+  String name;
+  String title;
+  Code status;
+  bool experimental;
+  DateTime date;
+  String publisher;
+  List<ContactDetail> contact;
+  String description;
+  List<UsageContext> useContext;
+  List<CodeableConcept> jurisdiction;
+  bool immutable;
+  String purpose;
+  String copyright;
+  Compose compose;
+  Expansion expansion;
+}
+
+class DomainResource extends Resource {
+  Narrative text;
+  List<Resource> contained;
+  List<Extension> extension;
+  List<Extension> modifierExtension;
+}
+
+class Narrative {
+  Code status;
+  String div;
+}
+
+class Compose extends BackboneElement {
+  DateTime lockedDate;
+  bool inactive;
+  List<Include> include;
+}
+
+class Expansion extends BackboneElement {
+  Identifier identifier;
+  DateTime timestamp;
+  int total;
+  int offset;
+  Parameter parameter;
+  Contains contains;
+}
+
+class Filter extends BackboneElement {
+  Code property;
+  Code op;
+  String value;
+}
+
+class Include extends BackboneElement {
+  String system;
+  String version;
+  Concept concept;
+  Filter filter;
+}
+
+class Designation extends BackboneElement {
+  Code language;
+  Coding use;
+  String value;
+}
+
+class Concept extends BackboneElement {
+  Code code;
+  String display;
+  List<Designation> designation;
+}
+
+class Contains extends BackboneElement {
+  String system;
+  bool abstract;
+  bool inactive;
+  String version;
+  Code code;
+  String display;
+  List<Designation> designation;
+  List<Contains> contains;
+}
+
+class FHIRRange extends FHIRElement {
+  int low;
+  int high;
+}
+
+class UsageContext extends FHIRElement {
+  Coding code;
+  CodeableConcept valueCodeableConcept;
+  Quantity valueQuantity;
+  FHIRRange valueRange;
+}
+
+class Parameter extends BackboneElement {
+  String name;
+  String valueString;
+  bool valueBoolean;
+  int valueInteger;
+  double valueDecimal;
+  String valueUri;
+  Code valueCode;
+  DateTime valueDateTime;
+}
+
+class AnswerOption {
+  DateTime valueDate;
+  String valueTime;
+  String valueString;
+  Coding valueCoding;
+  Reference valueReference;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- self_tracking_app/lib/main.dart
+++ self_tracking_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
+
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -44,17 +46,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
@@ -65,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -74,38 +69,36 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+          TextFormField(
+          validator: (value) {
+        if (value.isEmpty) {
+        return 'Please enter some text';
+        }
+        return null;
+        },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false
+              // otherwise.
+              if (_formKey.currentState.validate()) {
+                // If the form is valid, display a Snackbar.
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+            },
+            child: Text('Submit')
+          ),
+        )
+          ]
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    )));
   }
 }
