328da9d71aded296c9df9fd2bbcc4c89e93f71f0
ad1306 <adityadave.dave89@gmail.com>
2020-08-28 03:58:56 -0400

    added controls for primitive types for dstu3 questionnaire

--- questionnaire-generator/src/app/app.module.ts
+++ questionnaire-generator/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from "./app.component";
 import { R4Component } from './components/r4/r4.component';
 import { Dstu3Component } from './components/dstu3/dstu3.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, R4Component, Dstu3Component, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, ClarityModule],
+  imports: [BrowserModule, AppRoutingModule, ClarityModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- questionnaire-generator/src/app/components/dstu3/dstu3.component.ts
+++ questionnaire-generator/src/app/components/dstu3/dstu3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'; 
 
 @Component({
   selector: 'app-dstu3',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dstu3.component.scss']
 })
 export class Dstu3Component implements OnInit {
-
   constructor() { }
+
+  questionnaireForm = new FormGroup({
+    url: new FormControl(''),
+    version: new FormControl(''),
+    name: new FormControl(''),
+    title: new FormControl(''),
+    status: new FormControl(''),
+    experimental: new FormControl(''),
+    date: new FormControl(''),
+    publisher: new FormControl(''),
+    description: new FormControl(''),
+    purpose: new FormControl(''),
+    approvalDate: new FormControl(''),
+    lastReviewDate: new FormControl('')
+  })
 
   ngOnInit(): void {
   }
